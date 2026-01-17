ea25b7fad741501b65de5ac92f8e3f0ddbaaef99
mahamsheikh18 <mahamshek5@gmail.com>
2026-01-17 23:17:05 +0500

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,8 @@
+# Program to calculate the area of a circle
+
+pi = 3.14159
+radius = float(input("Enter the radius of the circle: "))
+
+area = pi * radius * radius
+
+print("Area of the circle is:", area)
---
+++ square_area.py
@@ -0,0 +1,7 @@
+# Program to calculate the area of a square
+
+side = float(input("Enter the side length of the square: "))
+
+area = side * side
+
+print("Area of the square is:", area)
